002e0bac05854ab9ad9b3093eebd2e2e5f1a834b
kaushikpaul34 <kaushikpal029@gmail.com>
2023-05-25 20:52:13 +0530

    new

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,24 @@
 
 print('Adding all the data processing code related fraud detection')
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print("sum function")
+
+def multiplication(a,b):
+    d=a*b
+    return b
+
+print("multiplication function")
+
+
+def duivision(a,b):
+    e=a/b
+    return e
+
+print("divisioon function")
